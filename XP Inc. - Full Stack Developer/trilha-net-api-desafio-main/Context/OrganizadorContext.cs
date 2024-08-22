8aa2eb1b984158c27da34cabcb60a8b526692377
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-08-22 10:16:23 -0300

    Desafio API

---
+++ XP Inc. - Full Stack Developer/trilha-net-api-desafio-main/Context/OrganizadorContext.cs
@@ -0,0 +1,15 @@
+using TrilhaApiDesafio.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrilhaApiDesafio.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
